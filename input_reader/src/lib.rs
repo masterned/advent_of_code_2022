857a209bad6396af283a80948b9d8311554ba0aa
Spencer Dent <mr.spencerdent@gmail.com>
2022-12-04 23:15:15 -0500

    refactor: use char instead of str for line split

--- input_reader/src/lib.rs
+++ input_reader/src/lib.rs
@@ -3,7 +3,7 @@ use std::{fs, io};
 pub fn read_file_as_lines(path: &str) -> io::Result<Vec<String>> {
     let contents = fs::read_to_string(path)?;
 
-    let lines = contents.trim().split("\n").map(String::from).collect();
+    let lines = contents.trim().split('\n').map(String::from).collect();
 
     Ok(lines)
 }
