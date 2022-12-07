555e9cae22f96c5a819f815993a24d5909f20e97
Spencer Dent <mr.spencerdent@gmail.com>
2022-12-07 00:27:52 -0500

    feat: solve day6:part2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -7,5 +7,8 @@ fn main() -> io::Result<()> {
     let start_of_packet = find_start_of_packet(&input);
     println!("Part 1: {start_of_packet}");
 
+    let start_of_message = find_start_of_message(&input);
+    println!("Part 2: {start_of_message}");
+
     Ok(())
 }
--- day6/src/tuning_trouble.rs
+++ day6/src/tuning_trouble.rs
@@ -1,12 +1,25 @@
-pub fn find_start_of_packet(data: &String) -> usize {
-    for (index, window) in data.chars().collect::<Vec<char>>().windows(4).enumerate() {
+fn find_start(length: usize, data: &String) -> usize {
+    for (index, window) in data
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(length)
+        .enumerate()
+    {
         let mut w_vec = window.to_vec();
         w_vec.sort();
         w_vec.dedup();
-        if w_vec.len() == 4 {
-            return index + 4;
+        if w_vec.len() == length {
+            return index + length;
         }
     }
 
     0
+}
+
+pub fn find_start_of_packet(data: &String) -> usize {
+    find_start(4, data)
+}
+
+pub fn find_start_of_message(data: &String) -> usize {
+    find_start(14, data)
 }
